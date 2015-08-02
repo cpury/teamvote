7325ccaa27866c4859f7ed2e8479fd6cf4fae6c4
Max <phraine@gmail.com>
2015-08-02 15:59:27 +0700

    Move ensure index code to server side

--- server/server.js
+++ server/server.js
@@ -1,3 +1,10 @@
+Meteor.startup(function () {
+  Ideas._ensureIndex({"author": 1});
+  Ideas._ensureIndex({"upvotes": 1});
+  Ideas._ensureIndex({"comments.author": 1});
+  Ideas._ensureIndex({"comments.upvotes": 1});
+});
+
 Meteor.publish("ideas", function () {
   return Ideas.find({});
 });
