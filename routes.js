606e070fc0414066d1ce7872fe18b97d97989a79
Max <phraine@gmail.com>
2015-08-03 22:00:28 +0700

    When routing, scroll back to top

--- routes.js
+++ routes.js
@@ -1,17 +1,20 @@
 FlowRouter.route('/', {
   action: function() {
     BlazeLayout.render('mainLayout', { main: "listIdeas" });
+    document.body.scrollTop = document.documentElement.scrollTop = 0;
   }
 });
 
 FlowRouter.route('/about/', {
   action: function() {
     BlazeLayout.render('mainLayout', { main: "staticAbout" });
+    document.body.scrollTop = document.documentElement.scrollTop = 0;
   }
 });
 
 FlowRouter.route('/contact/', {
   action: function() {
     BlazeLayout.render('mainLayout', { main: "staticContact" });
+    document.body.scrollTop = document.documentElement.scrollTop = 0;
   }
 });
