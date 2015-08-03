a13e3889250c79d7451a30b772ab964b5056ac94
Max <phraine@gmail.com>
2015-08-03 22:11:54 +0700

    Close menu fo reals

--- client/layout/layout.js
+++ client/layout/layout.js
@@ -1,5 +1,5 @@
 Template.navbar.onRendered(function() {
-  $(document).on('click','.navbar-collapse.in .navbar-header', function(e) {
+  $(document).on('click','.navbar-collapse.in', function(e) {
     if($(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle') {
       $(this).collapse('hide');
     }
