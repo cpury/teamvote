cd5e769ae364f6fe5abcb8f1a5acc2dff76d4b0c
Max <phraine@gmail.com>
2015-08-21 12:43:14 +0200

    Observe new ideas and push alerts

--- client/application.js
+++ client/application.js
@@ -10,10 +10,16 @@ Meteor.startup(function () {
   sAlert.config({
     effect: 'slide',
     position: 'top-right',
-    timeout: 3500,
+    timeout: 5000,
     html: false,
     onRouteClose: false,
     stack: true,
     offset: 10
+  });
+
+  Ideas.find({}).observeChanges({
+    added: function (id, idea) {
+      sAlert.info("New idea \"" + idea.title + "\" by " + idea.authorName);
+    }
   });
 });
